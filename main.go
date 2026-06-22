f83e0529ecf7ed6e9f13f45c5617208ad0ea2b83
iziangzen1684 <278657977+iziangzen1684@users.noreply.github.com>
2026-06-22 11:28:59 +0700

    Fix some wording.

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Set log flag to 0 and output to stderr
+	log.SetOutput(os.Stderr)
+	log.SetFlags(0)
+
+	parse_arg()
+	if help == true {
+		helper()
+	}
+
+	// Additional check
+	if !file_exists(data_path) {
+		log.Fatalf("File %v doesn't exist\n", data_path)
+	}
+
+	// Retrieve file content
+	file, err := os.ReadFile(data_path)
+	if err != nil {
+		log.Fatalf("Can't read file %v: %v\n", data_path, err)
+	}
+
+	data := string(file)
+
+	// Main table logic
+	markov_table := create_markov_table()
+	train_markov_table(data, markov_table)
+	fmt.Println(strings.Join(generate_text(markov_table, token_count), ""))
+}
